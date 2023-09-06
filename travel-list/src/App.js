27dbf1d76e88beee68f1179eeef4edae9054c60f
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-06 10:17:58 -0700

    gashaw gedef

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -20,13 +20,22 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(e);
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
       <select>
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-        <option value={3}>3</option>
+        {Array.from({length:20},(_,i)=>i+1).map
+        ((num)=>(
+          <option value={num} key={num}>
+            {num}
+
+          </option>
+
+          ))}
       </select>
       <input type="text" placeholder="Item..."/> 
       <button>Add</button>
@@ -39,7 +48,7 @@ function PackagingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item}  key={item.id}/>
         ))}
       </ul>
     </div>
